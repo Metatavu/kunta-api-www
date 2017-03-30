c5004f0300413263ca94f79de8fc63b17ed381b8
Antti Leppä <antti.leppa@metatavu.fi>
2017-03-30 07:28:54 +0300

    Added support for listing news articles by tag

--- modules/news/index.js
+++ modules/news/index.js
@@ -12,6 +12,53 @@
       this.parent = parent;
       this.newsApi = new parent.api.NewsApi();
     }
+    
+    listByTag(tag) {
+      this.parent.addPromise(new Promise((resolve) => {
+        this.newsApi.listOrganizationNews(this.parent.organizationId, {
+          tag: tag
+        }).then(news => {
+          var imagePromises = news.map(newsArticle => {
+            return this.newsApi.listOrganizationNewsArticleImages(
+              this.parent.organizationId,
+              newsArticle.id);
+          });
+
+          var results = _.cloneDeep(news);
+
+          async.eachOf(results, (result, index, callback) => {
+            var imagePromise = imagePromises[index];
+            imagePromise.then((imageResponse) => {
+              var basePath = this.parent.basePath;
+              var organizationId = this.parent.organizationId;
+              var newsArticleId = result.id;
+              var imageSrc = null;
+              if (imageResponse.length) {
+                imageSrc = util.format('%s/organizations/%s/news/%s/images/%s/data',
+                  basePath,
+                  organizationId,
+                  newsArticleId,
+                  imageResponse[0].id);
+              }
+
+              result.imageSrc = imageSrc;
+              callback();
+            }).catch((imageError) => {
+              console.error('Error loading news image', imageError);
+              callback();
+            });
+
+          }, () => {
+            resolve(results);
+          });
+        }).catch(listErr => {
+          console.error(util.format('Failed to list news article by tag %s', tag), listErr);
+          resolve([]);
+        });
+      }));
+
+      return this.parent;
+    } 
 
     findBySlug(slug) {
       this.parent.addPromise(new Promise((resolve) => {
